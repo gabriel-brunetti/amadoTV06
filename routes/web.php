e12cb1430dc8732c94a8685ab982f12f734eb6c0
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2020-07-30 20:10:26 -0300

    final da primeira aula

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/carrinho', function () {
+    return view('cart');
+});
+
+Route::get('/pagamento', function () {
+    return view('checkout');
+});
+
+Route::get('/produtos', function () {
+    return view('product-details');
+});
+
+Route::get('/shop', function () {
+    return view('shop');
+});
